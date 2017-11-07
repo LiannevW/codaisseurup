4636c11fa3035a2f532449ffd6bdef103a0737ee
Lianne van Westrhenen <lianne.van.westrhenen@gmail.com>
2017-11-07 13:51:01 +0100

    create default values event

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,6 +1,17 @@
 class Event < ApplicationRecord
   belongs_to :user
 
-validates :name, presence:true
-validates :discription, precence:true, length: { maximum: 500}
+attribute :price, :decimal, default: 0
+attribute :includes_food, :boolean, default: false
+attribute :include_drinks, :boolean, default: false
+attribute :active, :boolean, default: true
+
+validates :name, presence: true
+validates :discription, presence: true, length: { maximum: 500}
+validates :starts_at, presence: true
+validates :ends_at, presence: true
+
+
+#assignment +1 before_validation :
+
 end
